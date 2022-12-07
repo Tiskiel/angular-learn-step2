24a6afc1509f326971a54d0ac3453a1b1bcdd23b
steve <steve.lorent@bstorm.be>
2022-12-07 09:41:04 +0100

    menu

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -9,11 +9,23 @@ import { Link } from 'src/app/shared/models/link.model';
 export class NavComponent implements OnInit {
 
   listeLien : Link[] = [
-    {title : "Home", url : "home"}
+    {title : "Home", url : "home"},
+    {title : "Demo", children :  [
+      {title : "Bindings", url : "demo/demo1"},
+      {title : "Pipes", url : "demo/demo2"},
+      {title : "Directives", url : "demo/demo3"},
+    ]},
+    {title : "Exercices", children : [
+      {title : "Chronomètre", url : "exercice/exo1"}
+    ]}
   ]
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  switchChildrenVisible(index : number) {
+    this.listeLien[index].isChildrenVisible = !this.listeLien[index].isChildrenVisible
   }
 
 }
